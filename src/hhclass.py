b767b850515ac89867d072fafd5a9684c59606db
NasirovEF <i@ef-nasirov.ru>
2024-03-17 22:04:31 +0300

    Исправил процесс записи данных в файл в одну строчку

--- src/hhclass.py
+++ src/hhclass.py
@@ -18,7 +18,7 @@ class HeadHunterAPI(ABC):
         """Конструктор для класса"""
         self.url = url
 
-    def get_vacancy(self) -> dict:
+    def get_information(self) -> dict:
         """Метод для подключения через API к HH и получение вакансий"""
         response = requests.get(self.url)
         return response.json() #.get("items")
@@ -26,10 +26,10 @@ class HeadHunterAPI(ABC):
     def writing_to_file(self):
         """Записывает полученные вакансии в файл json"""
         with open("../data/apivacancy.json", "w", encoding="utf-8") as file:
-            json.dump(self.get_vacancy(), file, ensure_ascii=False)
+            json.dump(self.get_information(), file, indent=4, ensure_ascii=False)
 
     def __str__(self):
-        return f'Найдено {self.get_vacancy().get("found")} вакансий'
+        return f'Найдено {self.get_information.get("found")} вакансий'
 
 
 
