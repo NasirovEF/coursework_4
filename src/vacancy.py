96f9c6fb7cf0cffdf51a61d14d2af7d8c12d69b5
NasirovEF <i@ef-nasirov.ru>
2024-03-23 18:05:44 +0300

    add new class Vacancy with method writing_to_file(self)

---
+++ src/vacancy.py
@@ -0,0 +1,30 @@
+class Vacancy:
+
+    def __init__(self, vacancy):
+        """Конструктор для класса"""
+        self.name = vacancy.get('name')
+        self.url = vacancy.get('alternate_url')
+        if vacancy.get('address') is None:
+            self.address = "не указан"
+        else:
+            self.address = vacancy.get('address')['raw']
+        if vacancy.get('snippet')['requirement'] is None:
+            self.requirement = "не указаны"
+        else:
+            self.requirement = vacancy.get('snippet')['requirement']
+        self.responsibility = vacancy.get('snippet')['responsibility']
+        if vacancy.get('salary')['from'] is None:
+            self.__salary_from = 0
+        else:
+            self.__salary_from = vacancy.get('salary')['from']
+        self.__salary_to = vacancy.get('salary')['to']
+
+    def writing_to_file(self):
+        """Записывает полученные вакансии в файл"""
+        with open("../data/vacancies.txt", "a", encoding="utf-8") as file:
+            file.write(f'\n{self.__str__()}')
+
+    def __str__(self):
+        return (f'{self.name}\nЗарплата: {self.__salary_from} - {self.__salary_to}\n'
+                f'Адрес: {self.address}\nТребования: {self.requirement}\nОбязанности: {self.responsibility}\n'
+                f'Ссылка на вакансию {self.url}\n')
