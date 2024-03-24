394a915c4d3afd6c1a1d05259ffba25a62618b64
NasirovEF <i@ef-nasirov.ru>
2024-03-24 16:52:37 +0300

    Добавил нумерацию вакансий при записи в файл

--- src/vacancy.py
+++ src/vacancy.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class WritingVacancy(ABC):
 
     @abstractmethod
-    def writing_to_file(self, f):
+    def writing_to_file(self, n, f):
         pass
 
 
@@ -39,10 +39,10 @@ class Vacancy(WritingVacancy):
         """Геттер для верхней границы зарплаты"""
         return self.__salary_to
 
-    def writing_to_file(self, txt_file):
+    def writing_to_file(self, numb_vak, txt_file):
         """Записывает полученные вакансии в файл"""
         with open(txt_file, "a", encoding="utf-8") as file:
-            file.write(f'\n{self.__str__()}')
+            file.write(f'\n№{numb_vak} {self.__str__()}')
 
     def __str__(self):
         return (f'{self.name}\nЗарплата: {self.__salary_from} - {self.__salary_to}\n'
